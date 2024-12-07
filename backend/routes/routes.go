ceff9265b74c42f043d767d2c0fdf0b088437f8c
Aurorayyyyy <yanga262@gmail.com>
2024-12-08 01:26:56 +0700

    add the login route

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github/com/Aurorayyyyy/develop-agoda-interview-guessing-game/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	r.POST("/login", controllers.Login)
+
+	
+}
