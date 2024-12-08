aba1dbf73ecb92dcc7950d8a8e8633887e8adc67
Aurorayyyyy <yanga262@gmail.com>
2024-12-08 15:59:51 +0700

    change the default env

--- backend/config/database.go
+++ backend/config/database.go
@@ -35,8 +35,8 @@ func connectionDatabase() (*gorm.DB, error) {
 	username := getEnv("DB_USERNAME", "dev")
 	password := getEnv("DB_PASSWORD", "devpass")
 	host := getEnv("DB_HOST", "localhost")
-	port := getEnv("DB_PORT", "3307")
-	database := getEnv("DB_DATABASE", "localhost")
+	port := getEnv("DB_PORT", "3306")
+	database := getEnv("DB_DATABASE", "guessgame")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	fmt.Println(dsn)
