9b3432fb8de20c801581a3cbb2e4485bd3dfa41a
Aurorayyyyy <yanga262@gmail.com>
2024-12-09 00:20:00 +0700

    implement handle login and call API

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react"
+
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { useAuth } from "../context/AuthContext";
+
 export default function Login() {
+  const usernameRef = useRef('')
+  const passwordRef = useRef('')
+  const [error, setError] = useState('');
+  const navigator = useNavigate();
+  const { login } = useAuth()
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    const username = usernameRef.current
+    const password = passwordRef.current
+    const res = await login(username, password)
+    if (res) {
+      navigator('/guess')
+    } else {
+      setError("Wrong username or password")
+    }
+  }
     return (
-      <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
@@ -9,7 +30,7 @@ export default function Login() {
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
+            <form onSubmit={handleLogin} className="space-y-6">
               <div>
                 <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
                   Username
@@ -19,6 +40,7 @@ export default function Login() {
                     id="username"
                     name="username"
                     type="username"
+                    ref={ usernameRef }
                     required
                     autoComplete="username"
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
@@ -37,6 +59,7 @@ export default function Login() {
                     id="password"
                     name="password"
                     type="password"
+                    ref={ passwordRef }
                     required
                     autoComplete="current-password"
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
@@ -49,7 +72,7 @@ export default function Login() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Login in
+                  Login
                 </button>
                 <div className="text-sm text-right pr-4">
                     <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500 underline text-right">
@@ -58,10 +81,14 @@ export default function Login() {
                 </div>
               </div>
             </form>
+            <div className="text-sm text-right pr-4">
+              <p>
+                {error && <p className="text-lg font-bold text-center text-red-600">{error}</p>}
+              </p>  
+            </div>
   
           </div>
         </div>
-      </>
     )
   }
   
