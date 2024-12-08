b460f84d77786df6f6c13f759f760b7651d737a7
Aurorayyyyy <yanga262@gmail.com>
2024-12-09 00:19:23 +0700

    add auth context

--- frontend/src/Context/AuthContext.js
+++ frontend/src/Context/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import api from "../api/api";
 
 const AuthContext = React.createContext()
 
@@ -7,8 +8,32 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
+  const login = async (username, password) => {
+    try {
+      const res = await api.post('/login', {username, password})
+      const { token } = res.data
+      localStorage.setItem('token', token)
+      return true;
+    } catch (err) {
+      console.error('Login failed:', err);
+      return false;
+    }
+  }
+
+  const signup = async (username, password) => {
+    try {
+      await api.post('/signup', {username, password})
+      return true;
+    } catch (err) {
+      console.error('Signup failed:', err);
+      return false;
+    }
+  }
+  const logout = () => {
+    localStorage.removeItem('token')
+  }
 
   return(
-    <AuthContext.Provider value={localStorage.getItem("token")}>{props.children}</AuthContext.Provider>
+    <AuthContext.Provider value={{login, signup, logout}}>{children}</AuthContext.Provider>
   )
 }
