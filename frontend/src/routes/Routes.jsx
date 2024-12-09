65f67242b5de90f72fd40a78ef4b2c4a5229a55b
Aurorayyyyy <yanga262@gmail.com>
2024-12-09 17:25:40 +0700

    fix extensions import

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AuthorizedRoute from "./AuthorizedRoute.jsx";
 import UnauthorizedRoutes from "./UnauthorizedRoute.jsx";
-import Login from "../components/Login.js";
-import Signup from "../components/Signup.js";
-import Guess from "../components/Guess.js";
+import Login from "../components/Login.jsx";
+import Signup from "../components/Signup.jsx";
+import Guess from "../components/Guess.jsx";
 
 const Router = () => {
     return (
